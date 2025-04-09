037972bf31e3f7646314fa229610e8246b2ac44c
lizzysimmons1 <153117350+lizzysimmons1@users.noreply.github.com>
2025-04-08 22:53:02 -0600

    stytch beginnings

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { StytchProvider } from "@stytch/react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <StytchProvider config={stytchConfig}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StytchProvider>
   </React.StrictMode>
 );
+
+const stytchConfig = {
+  publicToken: process.env.REACT_APP_STYTCH_PUBLIC_TOKEN || "",
+  loginRedirectURL: "http://localhost:3000/dashboard",
+  sessionOptions: { duration_minutes: 60 },
+};
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
