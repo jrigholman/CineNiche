3d5c5a763b4b89063b5627f31ce1b861961cb11d
andiemasson <aclegg1@byu.edu>
2025-04-09 11:37:47 -0600

    fixed tables

--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07d2fdda13c169a3e21ecd0419cc1009bf414e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0772edc135b571d02caa091a4252e8eed36f396")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
